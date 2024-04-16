626d94f35974dacb8d58e6d71602b2c46456fe16
pedro <pedro@PEDRODRODRIGUES>
2024-04-16 11:30:09 +0100

    Adicionar arquivos de projeto.

---
+++ P3D_RayTracer_Template2/vector.h
@@ -0,0 +1,46 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+using namespace std;
+
+class Vector
+{
+public:
+	Vector(){};
+	Vector(float x, float y, float z);
+	Vector(const Vector& v);
+
+	float length();
+
+	float getAxisValue(int axis);
+
+	Vector&	normalize();
+	Vector operator=(const Vector& v);
+	Vector operator+( const Vector& v );
+	Vector operator-( const Vector& v );
+	Vector operator*( float f );
+	float  operator*(const Vector& v);   //inner product
+	Vector operator/( float f );
+	Vector operator%( const Vector& v); //external product
+	Vector&	operator-=	(const Vector& v);
+	Vector&	operator-=	(const float v);
+	Vector&	operator*=	(const float v);
+	Vector&	operator+=	(const float v);
+	
+	static float dot(const Vector& a, const Vector& b);  // Dot product
+	static Vector cross(const Vector& a, const Vector& b);  // Cross product
+
+	float x;
+	float y;
+	float z;
+
+     friend inline
+  istream&	operator >>	(istream& s, Vector& v)
+	{ return s >> v.x >> v.y >> v.z; }
+  
+};
+
+#endif
